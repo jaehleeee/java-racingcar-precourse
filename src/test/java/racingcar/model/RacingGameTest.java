208e57a58913ca8b809183ecd528b1805a982ae2
jaehlee <jaeh.lee@navercorp.com>
2022-10-09 19:01:42 +0900

    feat : 레이싱 경주 기능(랜덤 숫자를 통한이동)과 결과 저장 기능
    racing 기능 테스트 추가

--- src/test/java/racingcar/model/RacingGameTest.java
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -1,6 +1,8 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,9 +23,38 @@ class RacingGameTest {
         racingGame = new RacingGame(carList);
     }
 
+
+    @Test
+    @DisplayName("Racing 전진 동작 테스트")
+    void racingCanGoTest() {
+        Car testCar = new Car("testCar");
+
+        for (int i=0; i<10; i++) {
+            Integer raceNumber = Randoms.pickNumberInRange(0, 3);
+            racingGame.racing(testCar, raceNumber);
+        }
+
+        Assertions.assertThat(testCar.getGoCount())
+                .isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Racing 전진불가 동작 테스트")
+    void racingCannotGoTest() {
+        Car testCar = new Car("testCar");
+
+        for (int i=0; i<10; i++) {
+            Integer raceNumber = Randoms.pickNumberInRange(4, 9);
+            racingGame.racing(testCar, raceNumber);
+        }
+
+        Assertions.assertThat(testCar.getGoCount())
+                .isEqualTo(10);
+    }
+
     @Test
     @DisplayName("RacingGame 정상 동작 테스트")
-    void test() {
+    void racingGameToalTest() {
         racingGame.start(10);
         for (Car car : carList) {
             car.printRecord();
